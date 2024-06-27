413b21a17d80ac1e651da257e1928c8c4c5cd8ec
Bogdan <brasskazov@gmail.com>
2024-06-27 17:30:01 +0300

    fix: remove extra deps

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router/Router";
 
-import { queryClient } from "@/shared/api/queryClient";
 import "./App.scss";
 
 export const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  );
+  return <RouterProvider router={router} />;
 };
